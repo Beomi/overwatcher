0a66f3463e5bf4108a5b2b04198674957af3c5f4
Beomi <latheledusjp@gmail.com>
2016-09-22 10:37:26 +0900

    update server model

--- servers/models.py
+++ servers/models.py
@@ -12,11 +12,20 @@ class TimeStampModel(models.Model):
 class ServerInfo(TimeStampModel):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 
 class ServerNowStatus(TimeStampModel):
     server = models.ForeignKey('ServerInfo')
     status = models.ForeignKey('ServerStatus')
 
+    def __str__(self):
+        return self.server.name + self.status.status
+
 
 class ServerStatus(TimeStampModel):
-    status = models.CharField(max_length=4)
+    status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.status
