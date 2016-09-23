a4fceaf85eb995834a24e3bebc7c4ba633bc2e47
Beomi <latheledusjp@gmail.com>
2016-09-23 21:05:29 +0900

    add Core module folder

---
+++ core/views.py
@@ -0,0 +1,9 @@
+# pip modules
+import requests
+
+
+def _get_latest_board(url):
+    pass
+    # TODO: 오버워치 기술지원 > 토론장 > 인벤게시판 순서
+
+
--- servers/models.py
+++ servers/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-class TimeStampModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
+from core.models import TimeStampModel
 
 
 class ServerInfo(TimeStampModel):
